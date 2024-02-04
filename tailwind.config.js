ccbe815e602f28d51b50cfc194283bf59d592695
Daniel Richardo <danielrichardo103@gmail.com>
2024-02-04 16:00:40 +0700

    frist commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'regal-blue' : '#bfaa9f'
+      },
+      fontFamily: {
+        'default' : ['Noto Sans']
+      }
+    },
+  },
+  plugins: [],
+}
+
